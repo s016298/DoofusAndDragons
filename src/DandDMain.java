6cd2d6b32efd1710d3b14a835480400e3608ac07
ScandalousPorridge <s016298@students.lmsd.org>
2017-04-19 22:04:17 -0400

    Adds in data for four characters. They do not yet all have input or output streams. Note that since Ylu is a werebear type class he will need a seperate instance for his night form

--- src/DandDMain.java
+++ src/DandDMain.java
@@ -7,38 +7,65 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class DandDMain{
 
-	public static Character c;
+	public static Character c, c2, d, e, f;
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
-	boolean doesItExist = Files.exists(Paths.get("dAndD.txt"));
+	while (true){
+		boolean doesYluExist = Files.exists(Paths.get("Y'lu.txt"));
+	boolean doesCorvusExist = Files.exists(Paths.get("Corvus.txt"));
+	boolean doesRandyExist = Files.exists(Paths.get("SirRandy.txt"));
+	boolean doesDatBoiExist = Files.exists(Paths.get("DatBoi.txt"));
 	//below is reference for boolean statements in text, use for Item toString
 	//System.out.println(1+2==2?"yes":"no");
 	
-	
-	if (doesItExist){
-				FileInputStream fis = new FileInputStream("dAndD.txt");
+	/*checks to see if the file save file exists. If it does, the data from that file is inputed 
+	 * into the corresponding objects*/
+	if (doesYluExist){
+		/** create a file input steam for every character */	
+		FileInputStream fis = new FileInputStream("Y'lu.txt");
 				ObjectInputStream in = new ObjectInputStream(fis);
+				c = (Character)in.readObject();
+				
 				in.close();
 			
-				FileOutputStream fos = new FileOutputStream("dAndD.txt");
-				ObjectOutputStream out = new ObjectOutputStream(fos);
-			
-				out.writeObject(c);
-				out.close();
+				System.out.println(c);
+	/**create a file output steam for every character*/
+//				FileOutputStream fos = new FileOutputStream("Y'lu.txt");
+//				ObjectOutputStream out = new ObjectOutputStream(fos);
+//				out.writeObject(c);
+//				c = new Character("Corvus", 9, 11, 10, 14, 10, 63);
+//				out.writeObject(d);
+//				c = new Character("SirRandy", 9, 12, 10, 11, 12, 69);
+//				out.writeObject(e);
+//				c = new Character("DatBoi", 11, 12, 10, 9, 14, 67);
+//				out.writeObject(f);
+//				out.close();
 
 	}else{
 			Scanner s = new Scanner(System.in);
-			FileOutputStream fos = new FileOutputStream("dAndD.txt");
+			FileOutputStream fos = new FileOutputStream("Y'lu.txt");
+			FileOutputStream fos1 = new FileOutputStream("Corvus.txt");
+			FileOutputStream fos2 = new FileOutputStream("SirRandy.txt");
+			FileOutputStream fos3 = new FileOutputStream("DatBoi.txt");
 			ObjectOutputStream out = new ObjectOutputStream(fos);
 			//Character c;
-	
+			c = new Character("Y'lu", 12, 13, 10, 11, 10, 79);
 			out.writeObject(c);
+			c = new Character("Corvus", 9, 11, 10, 14, 10, 63);
+			out.writeObject(d);
+			c = new Character("SirRandy", 9, 12, 10, 11, 12, 69);
+			out.writeObject(e);
+			c = new Character("DatBoi", 11, 12, 10, 9, 14, 67);
+			out.writeObject(f);
 			out.close();
 
 			System.out.println(c);
+			System.out.println(d);
+			System.out.println(e);
+			System.out.println(f);
 	
 	}
 
-
+	}
 
 	
 
@@ -47,28 +74,7 @@ public class DandDMain{
 		//stupid events
 	/*Need to add getters for every stat for this to work*/	
 	
-	public static int turnsToStupidEvent(){
-		return (20-c.getRan())/2;
-	}
-	//random events
-	public static int strEvent(){
 
-		return c.getStr() + (int) Math.floor(Math.random()*c.getRan())+1;
-	}
-	public static int touEvent(){
-
-		return c.getTou() + (int) Math.floor(Math.random()*c.getRan())+1;
-	}
-	public static int spdEvent(){
-		Scanner s = new Scanner(System.in);
-
-		return c.getSpd() + (int) Math.floor(Math.random()*c.getRan())+1;
-	}
-	public static int intelEvent(){
-		Scanner s = new Scanner(System.in);
-
-		return c.getIntel() + (int) Math.floor(Math.random()*c.getRan())+1;
-	}
 	public static int rollDice(){
 		return (int) Math.floor(Math.random()*20+1);
 	}
